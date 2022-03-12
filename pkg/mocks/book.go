6116451871a5130da3675f2de00f14b1bd4cf6c9
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2022-03-12 19:57:16 +0100

    Implement book mock

---
+++ pkg/mocks/book.go
@@ -0,0 +1,12 @@
+package mocks
+
+import "github.com/chemicalstan/go-rest-crud/pkg/models"
+
+var Books = []models.Book{
+	{
+		Id:     1,
+		Title:  "GoLang",
+		Author: "Stan Ebuka",
+		Desc:   "Complete developer guide",
+	},
+}
